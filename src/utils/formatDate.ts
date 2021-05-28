b79db635e3336f0ff7b64ac40a720ae08faa1200
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-28 17:55:29 -0300

    removido console.log

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,6 +2,5 @@ import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export function formatDate(date: string, mask = 'dd MMM yyyy'): string {
-  console.log(date, mask);
   return format(new Date(date), mask, { locale: ptBR });
 }
